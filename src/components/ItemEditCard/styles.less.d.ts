bfc432877ae2847fcf3098f796b26e014788a8e8
KirillIvano <ivannadez@yandex.ru>
2020-05-11 01:20:21 +0300

    added edit card with icons

---
+++ src/components/ItemEditCard/styles.less.d.ts
@@ -0,0 +1,5 @@
+export const itemEditCard: string;
+export const itemName: string;
+export const itemControls: string;
+export const itemButton: string;
+export const itemIcon: string;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
 export {default as Button} from './Button';
+export {default as LayoutContainer} from './LayoutContainer';
+export {default as ErrorView} from './ErrorView';
+export {default as Preloader} from './Preloader';
+export {default as PageHeadline} from './PageHeadline';
+export {default as ItemEditCard} from './ItemEditCard';
