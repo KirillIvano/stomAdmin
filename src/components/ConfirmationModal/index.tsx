0826f2a50453c939b8bb50637761ef9abf02fae6
KirillIvano <ivannadez@yandex.ru>
2020-06-02 23:49:23 +0300

    added confirmation modal

--- src/components/ConfirmationModal/index.tsx
+++ src/components/ConfirmationModal/index.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
-import classnames from 'classnames';
+
+import {Modal, Button} from '@/uikit';
 
 import styles from './styles.less';
 
-const ConfirmationModal = () => {
-    return <div></div>;
-};
+type ConfirmationModalProps = {
+    headline: string;
+    content: string;
+    isOpen: boolean;
+
+    handleClose: () => void;
+    handleConfirm: () => void;
+}
+
+const ConfirmationModal = ({
+    headline,
+    content,
+    isOpen,
+
+    handleClose,
+    handleConfirm,
+}: ConfirmationModalProps) => (
+    <Modal
+        isOpen={isOpen}
+        handleClose={handleClose}
+        closable={false}
+    >
+        <h1 className={styles.headline}>{headline}</h1>
+        <p className={styles.content}>{content}</p>
+
+        <div className={styles.controlsBlock}>
+            <Button
+                className={styles.controlButton}
+                onClick={handleConfirm}
+            >
+                Подтвердить
+            </Button>
+            <Button
+                className={styles.controlButton}
+                onClick={handleClose}
+            >
+                Отмена
+            </Button>
+        </div>
+    </Modal>
+);
+
 
 export default ConfirmationModal;
---
+++ src/components/ConfirmationModal/styles.less.d.ts
@@ -0,0 +1,4 @@
+export const headline: string;
+export const content: string;
+export const controlsBlock: string;
+export const controlButton: string;
