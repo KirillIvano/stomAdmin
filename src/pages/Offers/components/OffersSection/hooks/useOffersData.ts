0e5e69ca2f98b9185f2639b175b633424e99d122
KirillIvano <ivannadez@yandex.ru>
2020-06-22 23:57:17 +0300

    added not effective modals controlling

--- src/pages/Offers/components/OffersSection/hooks/useOffersData.ts
+++
@@ -1,5 +0,0 @@
-import {useDataLoad} from '@/hooks/useDataLoad';
-import {getOffers} from '@/services/offers';
-
-export const useOffersData = (categoryId: number) =>
-    useDataLoad(() => getOffers(categoryId));
--- src/pages/Offers/hooks/useCategoriesData.ts
+++
@@ -1,4 +0,0 @@
-import {useDataLoad} from '@/hooks/useDataLoad';
-import {getCategories} from '@/services/offers';
-
-export const useCategoriesData = () => useDataLoad(getCategories);
