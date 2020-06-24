96b119f3ca070c1ed3d5a4074a99174abba64946
KirillIvano <ivannadez@yandex.ru>
2020-06-24 23:58:43 +0300

    added page wrappers

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,24 +3,28 @@ import {Route, Switch} from 'react-router-dom';
 
 import {PagePreloader} from '@/pages';
 
+import styles from './styles.less';
+
 const App = () => {
     return (
-        <Suspense fallback={<PagePreloader />}>
-            <Switch>
-                <Route
-                    path="/doctors"
-                    component={React.lazy(() => import('./../pages/Doctors/index'))}
-                />
-                <Route
-                    path="/offers"
-                    component={React.lazy(() => import('./../pages/Offers/index'))}
-                />
-                <Route
-                    path="/offersPreviews"
-                    component={React.lazy(() => import('./../pages/OfferPreviews/index'))}
-                />
-            </Switch>
-        </Suspense>
+        <div className={styles.pageWrapper}>
+            <Suspense fallback={<PagePreloader />}>
+                <Switch>
+                    <Route
+                        path="/doctors"
+                        component={React.lazy(() => import('./../pages/Doctors/index'))}
+                    />
+                    <Route
+                        path="/offers"
+                        component={React.lazy(() => import('./../pages/Offers/index'))}
+                    />
+                    <Route
+                        path="/offersPreviews"
+                        component={React.lazy(() => import('./../pages/OfferPreviews/index'))}
+                    />
+                </Switch>
+            </Suspense>
+        </div>
     );
 };
 
