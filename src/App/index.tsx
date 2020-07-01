b454d00922ccf4c6653ee1cc6d2ee24df07f00dd
KirillIvano <ivannadez@yandex.ru>
2020-07-01 23:57:51 +0300

    exact routing

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,29 +2,41 @@ import React, {Suspense} from 'react';
 import {Route, Switch} from 'react-router-dom';
 
 import {PagePreloader} from '@/pages';
+import {Header} from '@/parts';
 
 import styles from './styles.less';
 
 const App = () => {
     return (
-        <div className={styles.pageWrapper}>
-            <Suspense fallback={<PagePreloader />}>
-                <Switch>
-                    <Route
-                        path="/doctors"
-                        component={React.lazy(() => import('./../pages/Doctors/index'))}
-                    />
-                    <Route
-                        path="/offers"
-                        component={React.lazy(() => import('./../pages/Offers/index'))}
-                    />
-                    <Route
-                        path="/offersPreviews"
-                        component={React.lazy(() => import('./../pages/OfferPreviews/index'))}
-                    />
-                </Switch>
-            </Suspense>
-        </div>
+        <>
+            <Header />
+            <div className={styles.pageWrapper}>
+                <Suspense fallback={<PagePreloader />}>
+                    <Switch>
+                        <Route
+                            exact
+                            path="/"
+                            component={React.lazy(() => import('./../pages/Main/index'))}
+                        />
+                        <Route
+                            exact
+                            path="/doctors"
+                            component={React.lazy(() => import('../pages/Doctors/index'))}
+                        />
+                        <Route
+                            exact
+                            path="/doctor/:doctorId"
+                            component={React.lazy(() => import('../pages/Doctors/index'))}
+                        />
+                        <Route
+                            exact
+                            path="/offers"
+                            component={React.lazy(() => import('./../pages/Offers/index'))}
+                        />
+                    </Switch>
+                </Suspense>
+            </div>
+        </>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import 'babel-polyfill';
+import 'mobx-react-lite/batchingForReactDom';
 
 import './common/main.less';
 import App from './App';
